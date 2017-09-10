2c1104fb981b3d94ba2044a42d55671fc956bb72
Sunny Kumar Aditya <sunny.aditya@labs108.com>
2017-09-10 21:27:36 +0530

    step 6: adding a touch listener and adding circle when user taps.

--- app/src/main/java/app/circle/com/circle/CircleView.java
+++ app/src/main/java/app/circle/com/circle/CircleView.java
@@ -1,24 +1,58 @@
 package app.circle.com.circle;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by admin on 10/09/17.
  */
 
-public class CircleView extends View{
+public class CircleView extends View {
+
+    Paint paint;
+
     public CircleView(Context context) {
         super(context);
+        init(context);
     }
 
     public CircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
+
+    private void init(Context context) {
+        paint = new Paint();
+        paint.setColor(ContextCompat.getColor(context, R.color.colorAccent));
+        paint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(50, 50, 20, paint);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+
 }
--- app/src/main/java/app/circle/com/circle/MainActivity.java
+++ app/src/main/java/app/circle/com/circle/MainActivity.java
@@ -2,12 +2,42 @@ package app.circle.com.circle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        relativeLayout = (RelativeLayout) findViewById(R.id.rl_par);
+        relativeLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                switch (motionEvent.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        addCircle(motionEvent);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        break;
+                    case MotionEvent.ACTION_UP:
+                }
+                return false;
+            }
+        });
     }
+
+    private void addCircle(MotionEvent motionEvent) {
+
+        CircleView circleView = new CircleView(this);
+        relativeLayout.addView(circleView);
+    }
+
+
 }
