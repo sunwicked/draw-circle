edcfa9c4f825f0b9185ce2d39a341ea247ba3410
Sunny Kumar Aditya <sunny.aditya@labs108.com>
2017-09-10 20:27:03 +0530

    step 2 : Create a class for circle

---
+++ app/src/main/java/app/circle/com/circle/CircleView.java
@@ -0,0 +1,24 @@
+package app.circle.com.circle;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by admin on 10/09/17.
+ */
+
+public class CircleView extends View{
+    public CircleView(Context context) {
+        super(context);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
